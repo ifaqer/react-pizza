cbf096ba03ac5480357f885486fd7578eb22b8f0
ifaqer <super.mr.remix@gmail.com>
2024-02-26 18:17:49 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
 import React from 'react'
-import Axios from "axios"
-import {Route, Routes} from "react-router-dom"
 import './scss/app.scss'
-
-import Header from './components/Header'
-import Sorted from './components/Sorted'
-import Categories from './components/Categories'
-import PizzaBlock from './components/PizzaBlock'
-import Skeleton from './components/PizzaBlock/Skeleton'
-
-// rafc - создать быстро компонент!
+// rafce - создать быстро компонент!
 // CTRl + / = комментарий
+import Header from './components/Header'
+import Home from './pages/Home'
 
 export default function App() {
-  const [pizzas, setPizzas] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true)
-  React.useEffect(()=>{
-    Axios.get("https://65db02b53ea883a15290ffe7.mockapi.io/items").then((obj)=>{setPizzas(obj.data); setIsLoading(false)})
-  }, [])
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories/>
-            <Sorted/>
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-            isLoading ? [...new Array(8)].map((_, index)=><Skeleton key={index}/>) :
-            (pizzas.map((obj, index)=>(
-              <PizzaBlock {...obj} key={obj.id}/>
-            )))
-            }
-          </div>
+          <Home/>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Axios from "axios"
+
+import Sorted from '../components/Sorted'
+import Categories from '../components/Categories'
+import PizzaBlock from '../components/PizzaBlock'
+import Skeleton from '../components/PizzaBlock/Skeleton'
+
+export default function Home(){
+    const [pizzas, setPizzas] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+    React.useEffect(()=>{
+        Axios.get("https://65db02b53ea883a15290ffe7.mockapi.io/items").then((obj)=>{setPizzas(obj.data); setIsLoading(false)})
+    }, [])
+  return (
+    <>
+    <div className="content__top">
+        <Categories/>
+        <Sorted/>
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+        {
+        isLoading ? [...new Array(8)].map((_, index)=><Skeleton key={index}/>) :
+        (pizzas.map((obj, index)=>(
+            <PizzaBlock {...obj} key={obj.id}/>
+        )))
+        }
+    </div>
+    </>
+  )
+}
