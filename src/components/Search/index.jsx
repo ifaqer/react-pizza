3037065de5e972a84fc6ea947898d018e198768e
ifaqer <super.mr.remix@gmail.com>
2024-03-11 12:34:06 +0300

    deboucne

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styles from "./styles.module.scss"
 import {SearchContext} from "../../App"
-// import debounce from 'lodash.debounce'
+import debounce from "lodash.debounce"
 
 export default function Search(){
   const {search, setSearch} = React.useContext(SearchContext)
+  const [localValue, setLocaleValue] = React.useState('')
   const inputRef = React.useRef('')
-
+  const updateSearch = React.useCallback(
+    debounce((inputValue)=>{
+      setSearch(inputValue)
+    }, 600),
+    []
+  )
+  const onChangeInput = (event) => {
+    setLocaleValue(event.target.value)
+    updateSearch(event.target.value)
+  }
   const ClickClear = () => {
+    setLocaleValue('')
     setSearch('')
     inputRef.current.focus()
   }
@@ -15,7 +26,7 @@ export default function Search(){
   return (
     <div className={styles.root}>
     <label>
-    {search &&
+    {localValue &&
     <svg className="clear" onClick={ClickClear} width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#0F0F0F"/>
     </svg>}
@@ -23,7 +34,7 @@ export default function Search(){
     <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
     
-    <input ref={inputRef} value={search} onChange={(event)=>setSearch(event.target.value)} className={styles.input} type="text" placeholder="Поиск пиццы..."/>
+    <input ref={inputRef} value={localValue} onChange={(event)=>onChangeInput(event)} className={styles.input} type="text" placeholder="Поиск пиццы..."/>
     </label>
     </div>
   )
